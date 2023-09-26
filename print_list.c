ce3c08d47a47988ac690d51f15d7c494c7593f47
Phiwo Njemla <phiwo041590@gmail.com>
2023-09-26 03:09:02 -0700

    insertion_sort

---
+++ print_list.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list)
+
+{
+	int n;
+
+	n = 0;
+	while (list)
+	{
+		if (n > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++n;
+		list = list->next;
+	}
+	printf("\n");
+}
